f91f603cab6c25d86b0525a0f685d65216509de4
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2022-01-17 19:55:43 -0600

    update.

---
+++ script.js
@@ -0,0 +1,23 @@
+function displayTime() {
+	const dateTime = new Date();
+	let hrs = dateTime.getHours();
+	const min = dateTime.getMinutes();
+	const sec = dateTime.getSeconds();
+	const session = document.getElementById("session");
+
+	document.getElementById("hours").innerHTML = hrs;
+	document.getElementById("minutes").innerHTML = min;
+	document.getElementById("seconds").innerHTML = sec;
+
+	if (hrs >= 12) {
+		session.innerHTML = "PM";
+	}else {
+		session.innerHTML = "AM";
+	}
+
+	if (hrs > 12) {  // 12 hr format Ex: 8:00pm
+		hrs = hrs - 12; 
+	}
+
+}
+setInterval(displayTime,10);
